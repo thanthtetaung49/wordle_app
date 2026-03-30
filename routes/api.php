39ae386fa97b98eed35dc70e7cc6ebedb685a9cb
Thant Htet Aung <thanthtetaung12@gmail.com>
2026-03-30 11:28:53 +0630

    add: rate limiter to API end point

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::middleware('check.api.key')->group(function () {
+Route::middleware(['check.api.key', 'throttle:60,1'])->group(function () {
     Route::post('/set/wordle/answer', [WorldeApiController::class, 'setWordle']);
     Route::get('/player/activity/tracker', [WorldeApiController::class, 'playerTracker']);
     Route::get('/player/winner', [WorldeApiController::class, 'playerWinner']);
